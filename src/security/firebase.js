2d0c1ebf628bb0a63b907d1576e3e01d8a583270
beyarDev <beyarDev@gmail.com>
2022-07-08 23:02:42 +0100

    enhance security

--- src/security/firebase.js
+++ src/security/firebase.js
@@ -1,14 +1,13 @@
-import {initializeApp} from "firebase/app";
-import {getAuth} from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBaj-H8eH5dwDz0cCDdIzOBlYeiNzywxRc",
-  authDomain: "todo-app-168fb.firebaseapp.com",
-  projectId: "todo-app-168fb",
-  storageBucket: "todo-app-168fb.appspot.com",
-  messagingSenderId: "792346719343",
-  appId: "1:792346719343:web:e83f77d00fbf773db1099a",
+  apiKey: process.env.FapiKey,
+  authDomain: process.env.FauthDomain,
+  projectId: process.env.FprojectID,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.FappID,
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
