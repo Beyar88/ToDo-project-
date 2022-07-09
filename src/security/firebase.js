0fb0cc33f98fd873feece4b6118eb49589a812ea
beyarDev <beyarDev@gmail.com>
2022-07-09 20:29:38 +0100

    configure package.json

--- src/security/firebase.js
+++ src/security/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.FapiKey,
-  authDomain: process.env.FauthDomain,
-  projectId: process.env.FprojectID,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.FappID,
+  apiKey: process.env.REACT_APP_FapiKey,
+  authDomain: process.env.REACT_APP_FauthDomain,
+  projectId: process.env.REACT_APP_FprojectID,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_FappID,
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
